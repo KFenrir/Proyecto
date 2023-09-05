9a27417655df1bf5e9c10d407951bee20e9b1212
KFenrir <alarcon.luih99@gmail.com>
2023-09-04 21:01:33 -0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/home/home.page.html 	new file:   src/app/login/login-routing.module.ts 	new file:   src/app/login/login.module.ts 	new file:   src/app/login/login.page.html 	new file:   src/app/login/login.page.scss 	new file:   src/app/login/login.page.spec.ts 	new file:   src/app/login/login.page.ts 	new file:   src/app/restablecer/restablecer-routing.module.ts 	new file:   src/app/restablecer/restablecer.module.ts 	new file:   src/app/restablecer/restablecer.page.html 	new file:   src/app/restablecer/restablecer.page.scss 	new file:   src/app/restablecer/restablecer.page.spec.ts 	new file:   src/app/restablecer/restablecer.page.ts 	modified:   src/index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'restablecer',
+    loadChildren: () => import('./restablecer/restablecer.module').then( m => m.RestablecerPageModule)
+  },
 ];
 
 @NgModule({
